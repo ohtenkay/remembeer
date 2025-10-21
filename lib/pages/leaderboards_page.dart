15828f3cff9f3f9fe9a1bc1d906705fe56df1a12
Matej Klima <matej.klima5@gmail.com>
2025-10-21 23:08:32 +0200

    feat: proper page layout

---
+++ lib/pages/leaderboards_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardsPage extends StatelessWidget {
+  const LeaderboardsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Leaderboards Page'));
+  }
+}
