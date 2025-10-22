d4765e89755df8a32ee7af6c8725db9bffd45b01
Ondřej Hložek <hlozek.o1@gmail.com>
2025-10-23 00:06:27 +0200

    feat: mock beer list

--- lib/pages/drink_page.dart
+++ lib/pages/drink_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:remembeer/common/beer_icon.dart';
 
 class DrinkPage extends StatelessWidget {
   const DrinkPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Drink Page'));
+    return ListView.separated(
+      padding: EdgeInsets.all(12.0),
+      itemCount: 20,
+      separatorBuilder: (context, index) => Divider(),
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: BeerIcon(),
+          title: Text('Drink Item ${index + 1}'),
+          subtitle: Text('Details about Drink Item ${index + 1}'),
+          trailing: Icon(Icons.chevron_right),
+        );
+      },
+    );
   }
 }
