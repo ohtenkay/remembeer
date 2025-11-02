f319dde0fd0c8d1a132b5afd1ca6e18c6f3922d3
Matej Klima <matej.klima5@gmail.com>
2025-11-02 16:51:04 +0100

    feat: display drinks from db

---
+++ lib/drink/widget/drink_tile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:remembeer/common/beer_icon.dart';
+import 'package:remembeer/drink/model/drink.dart';
+
+class DrinkTile extends StatelessWidget {
+  final Drink drink;
+
+  const DrinkTile({super.key, required this.drink});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO(metju-ac): Implement a proper drink tile, this is just a placeholder.
+    return ListTile(
+      leading: BeerIcon(),
+      title: Text('Drink named ${drink.drink.name}'),
+      subtitle: Text(
+        'Drank on ${drink.timestamp.toLocal()} on ${drink.location?.latitude} ${drink.location?.longitude}',
+      ),
+      trailing: Icon(Icons.chevron_right),
+    );
+  }
+}
