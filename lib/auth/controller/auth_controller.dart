3cf10bd0c6cae9f44ac0fb7607aef621d1ca862a
Matej Klima <matej.klima5@gmail.com>
2025-11-04 16:56:39 +0100

    feat: auth controller

---
+++ lib/auth/controller/auth_controller.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AuthController {
+  final FirebaseAuth _firebaseAuth;
+  final _authStateSubject = BehaviorSubject<User?>();
+
+  AuthController(this._firebaseAuth) {
+    _authStateSubject.add(_firebaseAuth.currentUser);
+    _firebaseAuth.authStateChanges().listen(_authStateSubject.add);
+  }
+
+  late final Stream<User?> authStateChanges = _authStateSubject.stream;
+
+  User? get currentUser =>
+      _authStateSubject.valueOrNull ?? _firebaseAuth.currentUser;
+
+  bool get isAuthenticated => currentUser != null;
+
+  Future<UserCredential> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    return _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<UserCredential> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    return _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
--- lib/ioc/ioc_container.dart
+++ lib/ioc/ioc_container.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:remembeer/auth/controller/auth_controller.dart';
 import 'package:remembeer/drink/service/drink_controller.dart';
 
 final get = GetIt.instance;
@@ -8,5 +10,7 @@ class IoCContainer {
 
   static void initialize() {
     get.registerSingleton(DrinkController());
+    get.registerSingleton(FirebaseAuth.instance);
+    get.registerSingleton(AuthController(get<FirebaseAuth>()));
   }
 }
