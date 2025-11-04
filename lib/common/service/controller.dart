c2126e595a7c2993de2582088a24724e7bdb2e76
Ondřej Hložek <hlozek.o1@gmail.com>
2025-11-05 00:04:24 +0100

    feat: move controller, set protected properties and make contsructor use named properties

---
+++ lib/common/service/controller.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:remembeer/common/extension/json_firestore_helper.dart';
+import 'package:remembeer/common/model/entity.dart';
+import 'package:remembeer/common/model/value_object.dart';
+
+abstract class Controller<T extends Entity, U extends ValueObject> {
+  @protected
+  final CollectionReference<T> readCollection;
+  @protected
+  final CollectionReference<Map<String, dynamic>> writeCollection;
+
+  Controller({
+    required String collectionPath,
+    required T Function(Map<String, dynamic> json) fromJson,
+  }) : writeCollection = FirebaseFirestore.instance.collection(collectionPath),
+       readCollection = FirebaseFirestore.instance
+           .collection(collectionPath)
+           .withConverter(
+             fromFirestore: (snapshot, _) {
+               final json = snapshot.data() ?? {};
+               return fromJson(json.withId(snapshot.id));
+             },
+             toFirestore: (_, _) =>
+                 throw StateError('Invalid write to read only collection'),
+           );
+
+  Stream<List<T>> get entitiesStream => readCollection
+      .where(deletedAtField, isNull: true)
+      .snapshots()
+      .map(
+        (querySnapshot) => List.unmodifiable(
+          querySnapshot.docs.map((docSnapshot) => docSnapshot.data()).toList(),
+        ),
+      );
+
+  Future<void> createSingle(U dto) {
+    return writeCollection.add(dto.toJson().withServerCreateTimestamps());
+  }
+
+  Future<void> deleteSingle(T entity) {
+    return writeCollection
+        .doc(entity.id)
+        .set(entity.toJson().withoutId().withServerDeleteTimestamps());
+  }
+
+  Future<void> updateSingle(T entity) {
+    return writeCollection
+        .doc(entity.id)
+        .set(entity.toJson().withoutId().withServerUpdateTimestamp());
+  }
+}
--- lib/drink/service/drink_controller.dart
+++ lib/drink/service/drink_controller.dart
@@ -1,11 +1,11 @@
+import 'package:remembeer/common/service/controller.dart';
 import 'package:remembeer/drink/model/drink.dart';
 import 'package:remembeer/drink/model/drink_create.dart';
-import 'package:remembeer/drink/service/controller.dart';
 
 class DrinkController extends Controller<Drink, DrinkCreate> {
   DrinkController()
     : super(
-        'drinks',
-        Drink.fromJson,
+        collectionPath: 'drinks',
+        fromJson: Drink.fromJson,
       );
 }
