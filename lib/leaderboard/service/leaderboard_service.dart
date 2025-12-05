fd2907951d4d5b7ef7d05a81e1dc05e3d8626ff5
Matej Klima <matej.klima5@gmail.com>
2025-12-05 23:38:58 +0100

    feat: leaderboard service

---
+++ lib/leaderboard/service/leaderboard_service.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+
+import 'package:remembeer/auth/service/auth_service.dart';
+import 'package:remembeer/leaderboard/controller/leaderboard_controller.dart';
+import 'package:remembeer/leaderboard/model/leaderboard.dart';
+import 'package:remembeer/leaderboard/model/leaderboard_create.dart';
+
+const _inviteCodeLength = 8;
+const _inviteCodeChars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+
+class LeaderboardService {
+  final AuthService authService;
+  final LeaderboardController leaderboardController;
+  final _random = Random.secure();
+
+  LeaderboardService({
+    required this.authService,
+    required this.leaderboardController,
+  });
+
+  Future<Leaderboard?> findByInviteCode(String inviteCode) =>
+      leaderboardController.findByInviteCode(inviteCode);
+
+  Future<void> createLeaderboard(String name) async {
+    final inviteCode = await _generateUniqueInviteCode();
+    final currentUserId = authService.authenticatedUser.uid;
+
+    await leaderboardController.createSingle(
+      LeaderboardCreate(
+        name: name,
+        userIds: {currentUserId},
+        inviteCode: inviteCode,
+      ),
+    );
+  }
+
+  Future<void> updateLeaderboardName({
+    required String leaderboardId,
+    required String newName,
+  }) async {
+    final leaderboard = await leaderboardController.findById(leaderboardId);
+
+    if (leaderboard.name == newName) {
+      return;
+    }
+
+    final updatedLeaderboard = leaderboard.copyWith(name: newName);
+
+    await leaderboardController.updateSingle(updatedLeaderboard);
+  }
+
+  Future<void> joinLeaderboard(String leaderboardId) async {
+    final leaderboard = await leaderboardController.findById(leaderboardId);
+    final currentUserId = authService.authenticatedUser.uid;
+
+    if (leaderboard.userIds.contains(currentUserId)) {
+      return;
+    }
+
+    final updatedLeaderboard = leaderboard.copyWith(
+      userIds: {...leaderboard.userIds, currentUserId},
+    );
+
+    await leaderboardController.updateSingle(updatedLeaderboard);
+  }
+
+  Future<String> _generateUniqueInviteCode() async {
+    String code;
+    var isUnique = false;
+
+    do {
+      code = _generateRandomCode();
+      final existingLeaderboard = await leaderboardController.findByInviteCode(
+        code,
+      );
+      isUnique = existingLeaderboard == null;
+    } while (!isUnique);
+
+    return code;
+  }
+
+  String _generateRandomCode() {
+    return List.generate(
+      _inviteCodeLength,
+      (_) => _inviteCodeChars[_random.nextInt(_inviteCodeChars.length)],
+    ).join();
+  }
+}
