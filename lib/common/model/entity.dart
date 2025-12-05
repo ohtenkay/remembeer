0d10caf01dc3d49a8f92c0e1cffabe41ca6e65c4
Matej Klima <matej.klima5@gmail.com>
2025-12-05 09:56:55 +0100

    chore: add immutable tag

--- lib/common/model/entity.dart
+++ lib/common/model/entity.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:remembeer/common/model/timestamp_converter.dart';
 
+@immutable
 abstract class Entity {
   final String id;
   final String userId;
