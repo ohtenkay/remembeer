8ecad4a0c9d299ff7d101e4306a0a59cc5fb9cc6
Matej Klima <matej.klima5@gmail.com>
2025-12-07 00:39:55 +0100

    feat: standing stream

--- lib/leaderboard/service/leaderboard_service.dart
+++ lib/leaderboard/service/leaderboard_service.dart
@@ -4,18 +4,32 @@ import 'package:remembeer/auth/service/auth_service.dart';
 import 'package:remembeer/leaderboard/controller/leaderboard_controller.dart';
 import 'package:remembeer/leaderboard/model/leaderboard.dart';
 import 'package:remembeer/leaderboard/model/leaderboard_create.dart';
+import 'package:remembeer/leaderboard/model/leaderboard_entry.dart';
+import 'package:remembeer/leaderboard/service/month_service.dart';
+import 'package:remembeer/user/controller/user_controller.dart';
+import 'package:remembeer/user_stats/service/user_stats_service.dart';
 
 const inviteCodeLength = 8;
 const _inviteCodeChars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
 
 class LeaderboardService {
   final AuthService authService;
+  final MonthService monthService;
+
   final LeaderboardController leaderboardController;
+  final UserController userController;
+
+  // TODO(metju-ac): remove this after saving monthly stats in firestore
+  final UserStatsService userStatsService;
+
   final _random = Random.secure();
 
   LeaderboardService({
     required this.authService,
+    required this.monthService,
     required this.leaderboardController,
+    required this.userController,
+    required this.userStatsService,
   });
 
   Future<Leaderboard?> findByInviteCode(String inviteCode) =>
@@ -62,6 +76,70 @@ class LeaderboardService {
     );
 
     await leaderboardController.updateSingle(updatedLeaderboard);
+  }
+
+  bool isOwner(Leaderboard leaderboard) {
+    return leaderboard.userId == authService.authenticatedUser.uid;
+  }
+
+  Stream<List<LeaderboardEntry>> standingsStreamFor(Leaderboard leaderboard) {
+    return monthService.selectedMonthStream.asyncMap(
+      (selectedMonth) => _getStandings(
+        leaderboard: leaderboard,
+        year: selectedMonth.year,
+        month: selectedMonth.month,
+      ),
+    );
+  }
+
+  Future<List<LeaderboardEntry>> _getStandings({
+    required Leaderboard leaderboard,
+    required int year,
+    required int month,
+  }) async {
+    final entries = <LeaderboardEntry>[];
+
+    for (final userId in leaderboard.userIds) {
+      final user = await userController.userById(userId);
+      // TODO(metju-ac): Optimize this by storing the monthly stats in firestore
+      final stats = await userStatsService
+          .monthlyStatsStreamFor(userId: userId, year: year, month: month)
+          .first;
+
+      entries.add(
+        LeaderboardEntry(
+          user: user,
+          beersConsumed: stats.beersConsumed,
+          alcoholConsumedMl: stats.alcoholConsumedMl,
+          rankByBeers: 0,
+          rankByAlcohol: 0,
+        ),
+      );
+    }
+
+    entries.sort((a, b) => b.beersConsumed.compareTo(a.beersConsumed));
+    final byBeersRanks = <String, int>{};
+    for (var i = 0; i < entries.length; i++) {
+      byBeersRanks[entries[i].user.id] = i + 1;
+    }
+
+    entries.sort((a, b) => b.alcoholConsumedMl.compareTo(a.alcoholConsumedMl));
+    final byAlcoholRanks = <String, int>{};
+    for (var i = 0; i < entries.length; i++) {
+      byAlcoholRanks[entries[i].user.id] = i + 1;
+    }
+
+    return entries
+        .map(
+          (e) => LeaderboardEntry(
+            user: e.user,
+            beersConsumed: e.beersConsumed,
+            alcoholConsumedMl: e.alcoholConsumedMl,
+            rankByBeers: byBeersRanks[e.user.id]!,
+            rankByAlcohol: byAlcoholRanks[e.user.id]!,
+          ),
+        )
+        .toList();
   }
 
   Future<String> _generateUniqueInviteCode() async {
