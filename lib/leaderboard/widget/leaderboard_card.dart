016d860a73732f732ea371ffb66464b20ce6622b
Matej Klima <matej.klima5@gmail.com>
2025-12-07 01:26:44 +0100

    refactor: move onTap logic to leaderboard card

--- lib/leaderboard/widget/leaderboard_card.dart
+++ lib/leaderboard/widget/leaderboard_card.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:remembeer/leaderboard/model/leaderboard.dart';
+import 'package:remembeer/leaderboard/page/leaderboard_detail_page.dart';
 
 class LeaderboardCard extends StatelessWidget {
   final Leaderboard leaderboard;
-  final VoidCallback? onTap;
 
-  const LeaderboardCard({super.key, required this.leaderboard, this.onTap});
+  const LeaderboardCard({super.key, required this.leaderboard});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,14 @@ class LeaderboardCard extends StatelessWidget {
 
     return Card(
       child: ListTile(
-        onTap: onTap,
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<void>(
+              builder: (context) =>
+                  LeaderboardDetailPage(leaderboard: leaderboard),
+            ),
+          );
+        },
         leading: CircleAvatar(
           backgroundColor: theme.colorScheme.primaryContainer,
           child: Icon(
